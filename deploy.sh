05e17f0f7bcf0416bd59227927ff450bf31534b8
Arturo Saldivar <damonbalam@gmail.com>
2021-03-10 13:29:28 -0500

    fix deploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+
+php artisan key:generate
+
+#run Database Migrations
+php artisan migrate:fresh
+
+#seeders
+php artisan db:seed
+
+
+#pecl install swoole
+
+#php artisan vendor:publish --tag=laravel-swoole
+
+#php artisan swoole:http start
+
+
+
+#pecl install swoole
+
+#php bin/laravels start
